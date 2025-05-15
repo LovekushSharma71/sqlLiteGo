5a74d69aea526dc0f91fd99af309cd13b4b201d4
LovekushSharma71 <shirokuro7532@gmail.com>
2025-05-15 12:38:19 +0000

    added basic tree code and resolved merge conflicts

--- BTree/table.go
+++ BTree/table.go
@@ -5,14 +5,13 @@ import (
 	"db/DiskManager"
 	"encoding/binary"
 	"fmt"
+	"sort"
 )
 
 const (
-	BTREE_ORDER    = 4
-	MIN_NODES_ROOT = 1
-	MAX_NODES_ROOT = 3
-	MIN_NODES      = 1
-	MAX_NODES      = 3 
+	BTREE_ORDER = 4
+	MIN_NODES   = 1
+	MAX_NODES   = 3 
 	// MIN_NODES<=nodes<=MAX_NODES
 )
 
@@ -53,10 +52,54 @@ func (t *Table) Select(root *Page) {
 
 }
 
-func (t *Table) Insert(root *Page,key int64, val string) {
+func (t *Table) Insert(root *Page, addr DiskManager.DskAddr, key int64, val string) (DiskManager.DskAddr, *Node, DiskManager.DskAddr, DiskManager.DskAddr, error) {
 
-	
+	if root.PgHeader.IsLeaf {
 
+		if len(root.PageData) < MAX_NODES {
+
+			nde := append(root.PageData, Node{Len: int64(len(val)), Key: key, Val: val})
+			sort.Slice(nde, func(i, j int) bool {
+				return nde[i].Key < nde[i].Key
+			})
+
+			pge := Page{
+				PgHeader: Header{
+					IsLeaf: root.PgHeader.IsLeaf,
+					IsRoot: root.PgHeader.IsRoot,
+					NumNds: root.PgHeader.NumNds + 1,
+					Parent: root.PgHeader.Parent,
+				},
+				PageData: nde,
+				Children: root.Children,
+			}
+
+			err := t.Table.EditHeader(addr, DiskManager.HD_STAT, false)
+			if err != nil {
+				return -1, nil, -1, -1, err
+			}
+
+			data, err := SerializePage(&pge)
+			if err != nil {
+				return -1, nil, -1, -1, err
+			}
+			ddata := DiskManager.DiskData{
+				Header: DiskManager.RecordHeader{
+					Type: DiskManager.DT_BYTES,
+				},
+				Data: data,
+			}
+			err = t.Table.WrtDiskData(&ddata)
+			if err != nil {
+				return -1, nil, -1, -1, err
+			}
+
+			return ddata.Header.Addr, nil, -1, -1, nil
+
+		}
+
+	}
+	return -1, nil, -1, -1, nil
 }
 
 func DeserializePage(data []byte) (*Page, error) {
@@ -114,8 +157,8 @@ func DeserializePage(data []byte) (*Page, error) {
 }
 
 func SerializePage(pge *Page) ([]byte, error) {
-	if pge==nil{
-		return nil,fmt.Errorf("nil page error")
+	if pge == nil {
+		return nil, fmt.Errorf("nil page error")
 	}
 
 	var buf bytes.Buffer
